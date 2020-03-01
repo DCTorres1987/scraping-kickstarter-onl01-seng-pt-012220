dccd025ae08f50ce7ac1fc292fae58c85606405c
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-01 15:33:35 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,10 +1,9 @@
 # require libraries/modules here
 
 def create_project_hash
-  # write your code here
   html = File.read('fixtures/kickstarter.html')
- 
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
 
 create_project_hash
